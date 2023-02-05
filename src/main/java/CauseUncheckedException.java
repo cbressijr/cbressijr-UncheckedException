863431d15df5007f4158bbc4cfd807bcbee04fd2
Cosimo Bressi <cbressijr@msn.com>
2023-02-05 04:12:04 +0000

    Unchecked Exception Lab Complete

--- src/main/java/CauseUncheckedException.java
+++ src/main/java/CauseUncheckedException.java
@@ -1,5 +1,5 @@
 
-public class CauseUncheckedException {
+public class CauseUncheckedException extends RuntimeException{
     /**
      * This method must throw an unchecked exception.
      * unchecked means that you do not need to wrap the method in a try/catch or a throws declaration.
@@ -13,7 +13,8 @@ public class CauseUncheckedException {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
+    public void throwUncheckedException() {
+        throw new ArrayStoreException();
 
     }
 }
